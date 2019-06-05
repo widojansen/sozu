5f376d752e06f590d6e43090467f4019ae51a062
Geoffroy Couprie <geo.couprie@gmail.com>
2019-06-05 10:52:06 +0200

    use the new router and integrate in sozuctl

--- lib/examples/main.rs
+++ lib/examples/main.rs
@@ -9,7 +9,7 @@ use std::env;
 use std::io::stdout;
 use sozu_command::logging::{Logger,LoggerBackend};
 use sozu_command::proxy;
-use sozu_command::proxy::LoadBalancingParams;
+use sozu_command::proxy::{LoadBalancingParams,PathRule, RulePosition};
 use sozu_command::channel::Channel;
 
 fn main() {
@@ -37,10 +37,11 @@ fn main() {
   });
 
   let http_front = proxy::HttpFront {
-    app_id:     String::from("app_1"),
-    address:    "127.0.0.1:8080".parse().unwrap(),
-    hostname:   String::from("lolcatho.st"),
-    path_begin: String::from("/")
+    app_id:   String::from("app_1"),
+    address:  "127.0.0.1:8080".parse().unwrap(),
+    hostname: String::from("lolcatho.st"),
+    path:     PathRule::Prefix(String::from("/")),
+    position: RulePosition::Tree,
   };
 
   let http_backend = proxy::Backend {
@@ -110,10 +111,11 @@ fn main() {
   });
 
   let tls_front = proxy::HttpFront {
-    app_id:      String::from("app_1"),
-    address:     "127.0.0.1:8443".parse().unwrap(),
-    hostname:    String::from("lolcatho.st"),
-    path_begin:  String::from("/"),
+    app_id:   String::from("app_1"),
+    address:  "127.0.0.1:8443".parse().unwrap(),
+    hostname: String::from("lolcatho.st"),
+    path:     PathRule::Prefix(String::from("/")),
+    position: RulePosition::Tree,
   };
 
   command2.write_message(&proxy::ProxyRequest {
@@ -153,10 +155,11 @@ fn main() {
   });
 
   let tls_front2 = proxy::HttpFront {
-    app_id:      String::from("app_2"),
-    address:     "127.0.0.1:8443".parse().unwrap(),
-    hostname:    String::from("test.local"),
-    path_begin:  String::from("/"),
+    app_id:   String::from("app_2"),
+    address:  "127.0.0.1:8443".parse().unwrap(),
+    hostname: String::from("test.local"),
+    path:     PathRule::Prefix(String::from("/")),
+    position: RulePosition::Tree,
   };
 
   command2.write_message(&proxy::ProxyRequest {
